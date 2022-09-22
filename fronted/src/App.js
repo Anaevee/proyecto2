8900e47c584656fa5df122daa9b9315e12a2034a
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2022-09-22 22:36:15 +0200

    articulo funcionando y ruteo a categorias

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import  MainPage  from './pages/MainPage';
-import SecondMain  from './pages/SecondPage';
-import {Header} from './Component/Header';
+import MainPage from './pages/MainPage';
+import SecondMain from './pages/SecondPage';
+import { Header } from './Component/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ArticuloMain} from './pages/Articulo';
-
-
+import { ArticuloMain } from './pages/Articulo';
+import SecondPage from './pages/SecondPage';
 
 function App() {
   return (
-    <div className="App">
-     <BrowserRouter>
-     <Header/>
-     <Routes>
-            <Route path= '/producto/:idProduct' element = {<ArticuloMain />} />
-            <Route path='/search' element = {<MainPage/>} />
-            <Route path='/carrito' element = {<MainPage/>} />
-            <Route path='/' element={<MainPage />} />
-      </Routes>
-
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/producto/:idProduct' element={<ArticuloMain />} />
+          <Route path='/search' element={<MainPage />} />
+          <Route path='/categorias' element={<SecondPage />} />
+          <Route path='/carrito' element={<MainPage />} />
+          <Route path='/' element={<MainPage />} />
+        </Routes>
       </BrowserRouter>
-    
     </div>
   );
 }
