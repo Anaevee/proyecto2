b7e6a7fec504c4c5f95a851fbcad62c2057852ac
Anaevee <evee_mcm@hotmail.com>
2022-12-06 13:37:00 +0100

    parteCarrito

--- fronted/src/Component/CarritoCompra.js
+++ fronted/src/Component/CarritoCompra.js
@@ -25,47 +25,47 @@ export const CarritoCompra = () => {
 return (
 
 
-  <section class="h-100 gradient-custom">
-  <div class="container py-5">
-    <div class="row d-flex justify-content-center my-4">
+  <section className="h-100 gradient-custom">
+  <div className="container py-5">
+    <div className="row d-flex justify-content-center my-4">
       {interes.length >0 &&
-      interes.map(()=>{
+      interes.map((value,i)=>{
         return(
-        <CarritoCard></CarritoCard>
+        <CarritoCard key={i} value={value}></CarritoCard>
         )
 
       }
       )
       }
-      <div class="col-md-4">
-        <div class="card mb-4">
-          <div class="card-header py-3">
-            <h5 class="mb-0">Summary</h5>
+      <div className="col-md-4">
+        <div className="card mb-4">
+          <div className="card-header py-3">
+            <h5 className="mb-0">Summary</h5>
           </div>
-          <div class="card-body">
-            <ul class="list-group list-group-flush">
+          <div className="card-body">
+            <ul className="list-group list-group-flush">
               <li
-                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
                 Products
                 <span>$53.98</span>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+              <li className="list-group-item d-flex justify-content-between align-items-center px-0">
                 Shipping
                 <span>Gratis</span>
               </li>
               <li
-                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                 <div>
                   <strong>Total amount</strong>
                   <strong>
-                    <p class="mb-0">(including VAT)</p>
+                    <p className="mb-0">(including VAT)</p>
                   </strong>
                 </div>
                 <span><strong>$53.98</strong></span>
               </li>
             </ul>
 
-            <button type="button" class="btn btn-primary btn-lg btn-block">
+            <button type="button" className="btn btn-primary btn-lg btn-block">
               Go to checkout
             </button>
           </div>
